6ece654c55564f2b76762846ba33976a4c3b5fd2
johnnydrama92 <james.ferrier01@gmail.com>
2018-04-15 21:58:14 +0100

    blog app added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    text = models.TextField()
